ed0551b23b349f9316ec13631fedb74f521ef0c4
Kyle McClellan <kylemcclellan3@gmail.com>
2023-03-20 18:55:19 -0500

    Options extensions: Static config

---
+++ Confluent.Kafka.DependencyInjection/Options/StaticConfig.cs
@@ -0,0 +1,33 @@
+﻿namespace Confluent.Kafka.Options;
+
+sealed class StaticConfig : IClientConfigProvider
+{
+    readonly IEnumerable<KeyValuePair<string, string>> producerConfig;
+    readonly IEnumerable<KeyValuePair<string, string>> consumerConfig;
+    readonly IEnumerable<KeyValuePair<string, string>> adminClientConfig;
+
+    public StaticConfig(
+        IEnumerable<KeyValuePair<string, string>> producerConfig,
+        IEnumerable<KeyValuePair<string, string>> consumerConfig,
+        IEnumerable<KeyValuePair<string, string>> adminClientConfig)
+    {
+        this.producerConfig = producerConfig;
+        this.consumerConfig = consumerConfig;
+        this.adminClientConfig = adminClientConfig;
+    }
+
+    public IEnumerator<KeyValuePair<string, string>> ForProducer<TKey, TValue>()
+    {
+        return this.producerConfig.GetEnumerator();
+    }
+
+    public IEnumerator<KeyValuePair<string, string>> ForConsumer<TKey, TValue>()
+    {
+        return this.consumerConfig.GetEnumerator();
+    }
+
+    public IEnumerator<KeyValuePair<string, string>> ForAdminClient()
+    {
+        return this.adminClientConfig.GetEnumerator();
+    }
+}
