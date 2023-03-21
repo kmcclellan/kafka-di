676a0605c4cdf58d083bb462c5d68d568e990df3
Kyle McClellan <kylemcclellan3@gmail.com>
2023-03-20 20:34:23 -0500

    Options extensions: Partitioning

---
+++ Confluent.Kafka.DependencyInjection/Options/PartitionerSetup.cs
@@ -0,0 +1,33 @@
+﻿namespace Confluent.Kafka.Options;
+
+sealed class PartitionerSetup : IClientBuilderSetup
+{
+    readonly PartitionerDelegate partitioner;
+    readonly string? topic;
+
+    public PartitionerSetup(PartitionerDelegate partitioner, string? topic)
+    {
+        this.partitioner = partitioner;
+        this.topic = topic;
+    }
+
+    public void Apply<TKey, TValue>(ProducerBuilder<TKey, TValue> builder)
+    {
+        if (this.topic != null)
+        {
+            builder.SetPartitioner(this.topic, this.partitioner);
+        }
+        else
+        {
+            builder.SetDefaultPartitioner(this.partitioner);
+        }
+    }
+
+    public void Apply<TKey, TValue>(ConsumerBuilder<TKey, TValue> builder)
+    {
+    }
+
+    public void Apply(AdminClientBuilder builder)
+    {
+    }
+}
