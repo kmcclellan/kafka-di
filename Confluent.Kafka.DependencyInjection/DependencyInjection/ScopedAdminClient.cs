bec2324abf7c916b6b62ada42cfefe69ce9273f4
Kyle McClellan <kylemcclellan3@gmail.com>
2023-03-20 21:40:15 -0500

    Register scoped clients

---
+++ Confluent.Kafka.DependencyInjection/DependencyInjection/ScopedAdminClient.cs
@@ -0,0 +1,146 @@
+﻿namespace Confluent.Kafka.DependencyInjection;
+
+using Confluent.Kafka.Admin;
+
+sealed class ScopedAdminClient : IAdminClient
+{
+    readonly IDisposable scope;
+    readonly IAdminClient inner;
+
+    public ScopedAdminClient(ClientScopeFactory initOptions)
+    {
+        this.scope = initOptions(out var options);
+        this.inner = options.CreateAdminClient();
+    }
+
+    public Handle Handle => this.inner.Handle;
+
+    public string Name => this.inner.Name;
+
+    public int AddBrokers(string brokers)
+    {
+        return this.inner.AddBrokers(brokers);
+    }
+
+    public Metadata GetMetadata(TimeSpan timeout)
+    {
+        return this.inner.GetMetadata(timeout);
+    }
+
+    public Metadata GetMetadata(string topic, TimeSpan timeout)
+    {
+        return this.inner.GetMetadata(topic, timeout);
+    }
+
+    public Task CreateTopicsAsync(IEnumerable<TopicSpecification> topics, CreateTopicsOptions? options = null)
+    {
+        return this.inner.CreateTopicsAsync(topics, options);
+    }
+
+    public Task DeleteTopicsAsync(IEnumerable<string> topics, DeleteTopicsOptions? options = null)
+    {
+        return this.inner.DeleteTopicsAsync(topics, options);
+    }
+
+    public Task CreatePartitionsAsync(
+        IEnumerable<PartitionsSpecification> partitionsSpecifications,
+        CreatePartitionsOptions? options = null)
+    {
+        return this.inner.CreatePartitionsAsync(partitionsSpecifications, options);
+    }
+
+    public Task<List<DeleteRecordsResult>> DeleteRecordsAsync(
+        IEnumerable<TopicPartitionOffset> topicPartitionOffsets,
+        DeleteRecordsOptions? options = null)
+    {
+        return this.inner.DeleteRecordsAsync(topicPartitionOffsets, options);
+    }
+
+    public GroupInfo ListGroup(string group, TimeSpan timeout)
+    {
+        return this.inner.ListGroup(group, timeout);
+    }
+
+    public List<GroupInfo> ListGroups(TimeSpan timeout)
+    {
+        return this.inner.ListGroups(timeout);
+    }
+
+    public Task<ListConsumerGroupsResult> ListConsumerGroupsAsync(ListConsumerGroupsOptions? options = null)
+    {
+        return this.inner.ListConsumerGroupsAsync(options);
+    }
+
+    public Task<DescribeConsumerGroupsResult> DescribeConsumerGroupsAsync(
+        IEnumerable<string> groups,
+        DescribeConsumerGroupsOptions? options = null)
+    {
+        return this.inner.DescribeConsumerGroupsAsync(groups, options);
+    }
+
+    public Task DeleteGroupsAsync(IList<string> groups, DeleteGroupsOptions? options = null)
+    {
+        return this.inner.DeleteGroupsAsync(groups, options);
+    }
+
+    public Task<List<ListConsumerGroupOffsetsResult>> ListConsumerGroupOffsetsAsync(
+        IEnumerable<ConsumerGroupTopicPartitions> groupPartitions,
+        ListConsumerGroupOffsetsOptions? options = null)
+    {
+        return this.inner.ListConsumerGroupOffsetsAsync(groupPartitions, options);
+    }
+
+    public Task<List<AlterConsumerGroupOffsetsResult>> AlterConsumerGroupOffsetsAsync(
+        IEnumerable<ConsumerGroupTopicPartitionOffsets> groupPartitions,
+        AlterConsumerGroupOffsetsOptions? options = null)
+    {
+        return this.inner.AlterConsumerGroupOffsetsAsync(groupPartitions, options);
+    }
+
+    public Task<DeleteConsumerGroupOffsetsResult> DeleteConsumerGroupOffsetsAsync(
+        string group,
+        IEnumerable<TopicPartition> partitions,
+        DeleteConsumerGroupOffsetsOptions? options = null)
+    {
+        return this.inner.DeleteConsumerGroupOffsetsAsync(group, partitions, options);
+    }
+
+    public Task<List<DescribeConfigsResult>> DescribeConfigsAsync(
+        IEnumerable<ConfigResource> resources,
+        DescribeConfigsOptions? options = null)
+    {
+        return this.inner.DescribeConfigsAsync(resources, options);
+    }
+
+    public Task AlterConfigsAsync(
+        Dictionary<ConfigResource, List<ConfigEntry>> configs,
+        AlterConfigsOptions? options = null)
+    {
+        return this.inner.AlterConfigsAsync(configs, options);
+    }
+
+    public Task<DescribeAclsResult> DescribeAclsAsync(
+        AclBindingFilter aclBindingFilter,
+        DescribeAclsOptions? options = null)
+    {
+        return this.inner.DescribeAclsAsync(aclBindingFilter, options);
+    }
+
+    public Task CreateAclsAsync(IEnumerable<AclBinding> aclBindings, CreateAclsOptions? options = null)
+    {
+        return this.inner.CreateAclsAsync(aclBindings, options);
+    }
+
+    public Task<List<DeleteAclsResult>> DeleteAclsAsync(
+        IEnumerable<AclBindingFilter> aclBindingFilters,
+        DeleteAclsOptions? options = null)
+    {
+        return this.inner.DeleteAclsAsync(aclBindingFilters, options);
+    }
+
+    public void Dispose()
+    {
+        this.inner.Dispose();
+        this.scope.Dispose();
+    }
+}
---
+++ Confluent.Kafka.DependencyInjection/KafkaServiceCollectionExtensions.cs
@@ -0,0 +1,48 @@
+namespace Confluent.Kafka;
+
+using Confluent.Kafka.DependencyInjection;
+using Confluent.Kafka.Options;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+
+using MSOptions = Microsoft.Extensions.Options.Options;
+
+/// <summary>
+/// Extensions to configure Kafka clients as services.
+/// </summary>
+public static class KafkaServiceCollectionExtensions
+{
+    /// <summary>
+    /// Adds <see cref="IProducer{TKey, TValue}"/>, <see cref="IConsumer{TKey, TValue}"/>, and <see cref="IAdminClient"/> to the services.
+    /// </summary>
+    /// <param name="services">The service collection.</param>
+    /// <returns>A builder for client options.</returns>
+    public static OptionsBuilder<KafkaClientOptions> AddKafkaClient(this IServiceCollection services)
+    {
+        if (services == null) throw new ArgumentNullException(nameof(services));
+
+        services.AddSingleton<ClientScopeFactory>(
+            provider =>
+            {
+                return (out KafkaClientOptions options) =>
+                {
+                    // Use shared scope for options dependencies (e.g. serializers, handlers).
+                    var scope = provider.CreateScope();
+
+                    options = scope.ServiceProvider
+                        .GetRequiredService<IOptionsFactory<KafkaClientOptions>>()
+                        .Create(MSOptions.DefaultName);
+
+                    return scope;
+                };
+            });
+
+        services.TryAddSingleton(typeof(IProducer<,>), typeof(ScopedProducer<,>));
+        services.TryAddSingleton(typeof(IConsumer<,>), typeof(ScopedConsumer<,>));
+        services.TryAddSingleton<IAdminClient, ScopedAdminClient>();
+
+        return services.AddOptions<KafkaClientOptions>();
+    }
+}
