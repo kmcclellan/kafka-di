6a77dc7919e05b98bb94f9216e3095e562197539
Kyle McClellan <kyle.mcclellan@icloud.com>
2024-02-19 23:12:43 -0600

    Parallel consumer

---
+++ Confluent.Kafka.DependencyInjection/Hosting/ParallelConsumeOptions.cs
@@ -0,0 +1,15 @@
+﻿namespace Confluent.Kafka.Hosting;
+
+/// <summary>
+/// Options for processing multiple Kafka messages/events concurrently.
+/// </summary>
+public class ParallelConsumeOptions
+{
+    /// <summary>
+    /// Gets or sets the maximum number of messages/events to process concurrently.
+    /// </summary>
+    /// <remarks>
+    /// Defaults to <see cref="Environment.ProcessorCount"/>. Set to <c>0</c> to disable message processing.
+    /// </remarks>
+    public int MaxDegreeOfParallelism { get; set; } = Environment.ProcessorCount;
+}
